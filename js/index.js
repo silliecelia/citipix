03ea11310038003fcde102ea2d3827ec53497b2a
silliecelia <chungsyc@gmail.com>
2016-03-06 11:12:14 -0500

    updates

--- js/index.js
+++ js/index.js
@@ -17,3 +17,22 @@ $( "form" ).on( "submit", function(event) {
 		$("body").addClass("sydney");
 	}
 });
+
+var cities = ['NYC', 'SF', "LA", "ATX", "SYD"];
+// console.log( cities[0] );
+// console.log( cities[1] );
+// console.log( cities[2] );
+
+for (var i = 0; i < cities.length; i++) {
+	console.log(cities[i]);
+	// <option val="cityname">cityname</option>
+	$( '#city-options' ).append( '<option>' + cities[i] + '</option>');
+
+}
+
+
+// function add(a,b){
+// 	return a + b;
+// }
+
+// console.log( add(1,2) );
